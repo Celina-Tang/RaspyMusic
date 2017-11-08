be3cdb315d0201e7628c0dd56690e7a5ad144a40
Celina Tang <tangcelina12@hotmail.ca>
2017-11-07 23:29:57 -0500

    used wtforms library to retrieve and validate data from form

--- raspyMusic.py
+++ raspyMusic.py
@@ -1,9 +1,35 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#import os
+#import RPi.GPIO as GPIO
+from flask import Flask, render_template, request, flash
+from wtforms import Form, TextField, TextAreaField, StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
-@app.route("/raspyMusic")
+#GPIO.setmode(GPIO.BCM)
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '5fe584s61fe849fs951f1s6e'
+
+
+class submitSongForm(Form):
+    songTitle = StringField('Song Title:', validators=[DataRequired()])
+    url = StringField('URL:', validators=[URL()])
+
+@app.route("/raspyMusic/", methods=['GET','POST'])
 def raspyMusic():
-    return render_template('/main.html')
+    form = submitSongForm(request.form)
+    if request.method == 'POST':
+        songTitle = request.form['songTitle']
+        url = request.form['url']
+        print(songTitle)
+        print(url)
+        if form.validate():
+            flash('Thanks for your submission. RPi will begin playback...')
+        #if url:
+            #os.system('mpg123 -q ' + url + ' &')
+    return render_template('submitSong.html', form=form)
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug = True)
